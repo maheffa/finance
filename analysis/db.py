70df4db5664e00927e6e1e8084ad7d8c924447e3
Adama Manitrarivo <adama.manitrarivo@booking.com>
2019-11-28 17:52:19 +0100

    Adding DB insert or update of tag data

--- analysis/db.py
+++ analysis/db.py
@@ -4,32 +4,35 @@ DB_FILE = 'data.db'
 
 TABLE_NAME = 'stock_info'
 
+
+def to_db_date(d):
+    return d.strftime('%Y-%d-%m 00:00:00')
+
+
 class DB:
+    STOCK_COLUMNS = ('close', 'open', 'high', 'low', 'volume')
+    TAG_COLUMNS = ('pricetoearnings')
+
     def __init__(self):
         self.conn = sqlite3.connect(DB_FILE)
         self._ensure_db()
 
     def _ensure_db():
         cur = self.conn.cursor()
-        tags = ['pricetoearnings']
+        columns = DB.STOCK_COLUMNS + DB.TAG_COLUMNS
         cur.executescript("""
             CREATE TABLE IF NOT EXISTS {} (
                 id INTEGER PRIMARY KEY,
                 identifier TEXT NOT NULL,
                 date TEXT NOT NULL,
-                close REAL,
-                open REAL,
-                high REAL,
-                low REAL,
-                volume REAL,
                 {},
                 UNIQUE(identifier, date)
             );
-        """.format(TABLE_NAME, ', '.join([tag + ' REAL' for tag in tags]))
+        """.format(TABLE_NAME, ', '.join([c + ' REAL' for c in columns]))
 
     def _find_ids(self, identifier, dates):
         ids = [-1 for _ in dates]
-        date_to_index = { date.strftime('%Y-%d-%m 00:00:00'): index for (index, date) in enumerate(dates)}
+        date_to_index = { to_db_date(date): index for (index, date) in enumerate(dates)}
         c = self.conn.cursor()
         for row in c.execute('SELECT id, date FROM {} WHERE identifier = {}'.format(TABLE_NAME, identifier)):
             if row[1] in date_to_index:
@@ -38,17 +41,29 @@ class DB:
         return ids
 
     def insert_or_update_stocks(identifier, stock_objects):
-        stock_ids = self._find_ids(identifier, [stock_object['date'] for stock_object in stock_objects])
-        columns = ()
-
-        enumerated_stocks = enumerate(stock_objects)
-        to_insert_list = [stock_object['value'] for (i, stock_object) in enumerated_stocks if stock_ids[i] == -1]
-        to_update_list = [(*stock_object['value'], stock_ids[i]) for (i, stock_object) if stock_ids[i] != -1]
+        ids = self._find_ids(identifier, [stock_object['date'] for stock_object in stock_objects])
+        to_insert_list = [(to_db_date(stock_object['date']), identifier, *stock_object['value'])
+                          for (i, stock_object) in enumerate(stock_objects) if ids[i] == -1]
+        to_update_list = [(*stock_object['value'], ids[i])
+                          for (i, stock_object) in enumerate(stock_objects) if ids[i] != -1]
 
         print('Inserting {} elements'.format(len(to_insert_list)))
-        self.conn.executemany('INSERT INTO {}(close,open,high,low,volume) VALUES (?, ?, ?, ?, ?)'.format(TABLE_NAME), to_insert_list)
+        self.conn.executemany('INSERT INTO {}(date,identifier,close,open,high,low,volume) VALUES (?, ?, ?, ?, ?)'.format(TABLE_NAME), to_insert_list)
         self.conn.commit()
 
         print('Updating {} elements'.format(len(to_update_list)))
         self.conn.executemany('UPDATE {} SET close=?,open=?,high=?,low=?,volume=? WHERE id=?'.format(TABLE_NAME), to_update_list)
+        self.conn.commit()
+
+    def insert_or_update_tag(identifier, tag, data):
+        ids = self._find_ids(identifier, [d['date'] for d in data])
+        to_insert_list = [(to_db_date(d['date']), identifier, d['value']) for (i, d) in enumerate(data) if ids[i] == -1]
+        to_update_list = [(d['value'], ids[i]) for (i, d) in enumerate(data) if ids[i] != -1]
+
+        print('Inserting {} elements'.format(len(to_insert_list)))
+        self.conn.executemany('INSERT INTO {}(date,identifier,{}) VALUES (?, ?, ?)'.format(TABLE_NAME, tag), to_insert_list)
+        self.conn.commit()
+
+        print('Updating {} elements'.format(len(to_update_list)))
+        self.conn.executemany('UPDATE {} SET {}=? WHERE id=?'.format(TABLE_NAME, tag), to_update_list)
         self.conn.commit()

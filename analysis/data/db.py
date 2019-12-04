33dd380f99d267fa6c6ab6541d41e5cdaf719b60
Adama Manitrarivo <adama.manitrarivo@booking.com>
2019-12-04 11:22:22 +0100

    Finalizing ORM

--- analysis/data/db.py
+++ analysis/data/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Query
 from sqlalchemy.sql.expression import bindparam
 
 from data.meta import session
-from data.model import TABLE_STOCK_INFO, TAG_COLUMNS, StockInfo, Company
+from data.model import TAG_COLUMNS, StockInfo, Company
 
 
 def to_db_date(d):
@@ -15,38 +15,6 @@ def to_db_date(d):
 class DB:
     def __init__(self):
         self.session = session
-
-    # def _ensure_db(self):
-    #     cur = self.conn.cursor()
-    #     columns = (*STOCK_COLUMNS, *TAG_COLUMNS)
-    #     cur.executescript("""
-    #         CREATE TABLE IF NOT EXISTS '{}' (
-    #             id INTEGER PRIMARY KEY,
-    #             identifier TEXT NOT NULL,
-    #             date TEXT NOT NULL,
-    #             {},
-    #             UNIQUE(identifier, date)
-    #         );
-    #
-    #         CREATE TABLE IF NOT EXISTS 'companies' (
-    #             id TEXT PRIMARY KEY,
-    #             identifier TEXT NOT NULL,
-    #             name TEXT NOT NULL,
-    #             sector TEXT,
-    #             industry_category TEXT,
-    #             industry_group TEXT,
-    #             stock_exchange TEXT,
-    #             created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime'))
-    #         );
-    #
-    #         CREATE TABLE IF NOT EXISTS '{}' (
-    #             id TEXT PRIMARY KEY,
-    #             {},
-    #         );
-    #     """.format(TABLE_STOCK_INFO,
-    #                ', '.join([c + ' REAL' for c in columns]),
-    #                TABLE_FETCH_LOG,
-    #                ', '.join([c + ' TEXT' for c in columns])))
 
     def _find_ids(self, identifier, dates):
         ids = [-1 for _ in dates]
@@ -79,6 +47,7 @@ class DB:
             [StockInfo.create_with_info(identifier, stock_object['date'], stock_object['value'])
              for (i, stock_object) in enumerate(stock_objects) if ids[i] == -1]
         )
+        self.session.commit()
 
     def insert_or_update_tag(self, identifier, tag, data):
         ids = self._find_ids(identifier, [d['date'] for d in data])
@@ -93,6 +62,7 @@ class DB:
               [tag]: d['value'],
               } for (i, d) in enumerate(data) if ids[i] == -1]
         )
+        self.session.commit()
 
     def populate_stocks(self, intrinio, identifier, start_date, end_date):
         self.insert_or_update_stocks(identifier, intrinio.get_stock_prices(identifier, start_date, end_date))
@@ -106,19 +76,20 @@ class DB:
         exists = set([s.id for s in query.all()])
         companies = [Company(c[0], c[1], c[2]) for c in all_companies if c[0] not in exists]
         self.session.bulk_save_objects(companies)
+        self.session.commit()
 
     def populate_missing_randomly(self, intrinio, n_random=500):
-        ids = [res.id for res in session.query(Company.id).filter(Company.sector is None)]
+        ids = [res.id for res in session.query(Company.id).filter(Company.sector.is_(None)).all()]
         shuffle(ids)
         print('Fetching {} companies'.format(n_random))
         companies_info = intrinio.get_extra_companies_info(ids[:n_random])
         print('Writing')
-        self.conn.executemany("""
-            UPDATE companies
-            SET sector=?, industry_category=?, industry_group=?, stock_exchange=?
-            WHERE id=?
-        """, [(s, i_c, i_g, s_e, i) for (i, s, i_c, i_g, s_e) in companies_info])
-        self.conn.commit()
+        self.session.bulk_update_mappings(
+            Company,
+            [{ 'id': i, 'sector': s, 'industry_category': i_c, 'industry_group': i_g, 'stock_exchange': s_e }
+             for (i, s, i_c, i_g, s_e) in companies_info]
+        )
+        self.session.commit()
 
     def populate_db(self, intrinio, stock_identifiers, start_date='1990-01-01', end_date=None):
         if end_date is None:
